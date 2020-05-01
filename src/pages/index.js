359803c3c8c10d0e607f937c87b5b3315fdff660
Jens Stavnstrup <stavnstrup@gmail.com>
2020-05-01 10:11:32 +0200

    Porting to Gatsby initial steps

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const IndexPage = () => (
+  <>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby based NISP Nation site.</p>
+  </>
+)
+
+export default IndexPage
