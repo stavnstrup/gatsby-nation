e95e96c0503f977684a69fb44409468054e1e32f
Jens Stavnstrup <stavnstrup@gmail.com>
2020-05-01 17:40:28 +0200

    add styling to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   const dayTime = new Date()
 
-  return <div>Last updated on {dayTime.toUTCString()}</div>
+  return <div className="footer">Last updated on {dayTime.toUTCString()}</div>
 }
 
 export default Footer
