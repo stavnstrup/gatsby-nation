c4fea45366d24141f3a81e89539910bc4b29f727
Jens Stavnstrup <stavnstrup@gmail.com>
2020-05-05 13:17:14 +0200

    First draft on node list page

---
+++ src/components/taxonomytree.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Taxonomytree = () => {
+  return <div>Hello tree</div>
+}
+
+export default Taxonomytree
--- src/pages/node.js
+++ src/pages/node.js
@@ -1,8 +1,13 @@
 import React from "react"
 import Element from "../components/element"
+import Taxonomytree from "../components/taxonomytree"
 
 const node = () => {
-  return <Element type="Taxonomy Nodes"></Element>
+  return (
+    <Element type="Taxonomy Nodes">
+      <Taxonomytree />
+    </Element>
+  )
 }
 
 export default node
